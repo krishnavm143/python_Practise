269748eb1dcbe99c6c43bc989697d59f738bf5f0
krishna <krishnavm143@gmail.com>
2023-01-18 11:24:08 +0530

    basic numpy examples

---
+++ NumPy/01_example.py
@@ -0,0 +1,16 @@
+# a=[1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,100]
+# b=[2,3,4,5,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,2,3,4,5]
+
+# for i in range(len(a)):
+#     c[i]=(a[i]*b[i])
+
+import numpy as np
+
+arr=np.array([[1,2,3],[4,7,5]])
+print(arr)
+print(arr.ndim)
+print(arr.shape)
+print(arr.size)
+print(arr.dtype)
+
+   
